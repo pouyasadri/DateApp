6bb14721be42d9123797c69faafc434daaa33eb1
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-04 01:14:52 +0200

    Initial Commit

---
+++ DateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateApp
+//
+//  Created by Pouya Sadri on 04/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateApp/DateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateAppApp.swift
+//  DateApp
+//
+//  Created by Pouya Sadri on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
